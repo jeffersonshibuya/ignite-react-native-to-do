e6dbcc76fa55f237102cfe214f671958d80d45c8
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2021-07-10 10:59:17 -0300

    Challenge done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,24 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    setTasks([...tasks, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const task = tasks.find(task => task.id === id) as Task
+    task.done = !task?.done
+    setTasks([...tasks])
+
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
@@ -26,10 +36,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
